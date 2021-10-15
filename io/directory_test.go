0b0f4cb238a2d6f7a3b15fb9ce958cf7e2239ef1
Lucas Molas <schomatis@gmail.com>
2021-10-15 09:46:37 -0300

    change package name: internal -> private

--- io/directory_test.go
+++ io/directory_test.go
@@ -18,7 +18,7 @@ import (
 
 	ft "github.com/ipfs/go-unixfs"
 	"github.com/ipfs/go-unixfs/hamt"
-	"github.com/ipfs/go-unixfs/internal/completehamt"
+	"github.com/ipfs/go-unixfs/private/completehamt"
 
 	"github.com/stretchr/testify/assert"
 )
